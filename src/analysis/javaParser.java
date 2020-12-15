887e921a2db502d5d6b5db574ed0d3e0b91e7cf3
FahdDebbiche <gusdebfa@student.gu.se>
2020-12-15 15:47:57 +0100

    add user input control

--- src/analysis/javaParser.java
+++ src/analysis/javaParser.java
@@ -3,22 +3,34 @@ package analysis;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class javaParser {
 	
 	public static void main(String[] args) throws IOException {
 		
 		//String sourceFolder = "./../../SoftwareEvolution/src/main/java" ;
-		
 				//try {
-					Path p1 = Paths.get("/Users/fahd/eclipse-workspace/SoftwareEvolution/group7/src/main/java/org/jabref/model");
+					Path p1 = Paths.get("/Users/fahd/Desktop/group7/src/main/java/org/jabref/model/groups");
 
 				//		if (args.length == 2) { 
 						// Get input
 						//String sourceFolder = args[0]; 
 						//String curDir = System.getProperty(sourceFolder);  // get the current files in the source folder
-					Parser parser = new Parser(p1.toString());
-					parser.javaToJSonFile();
+			        System.out.println("enter the number of files you want to include :");
+
+					try(Scanner studentInput = new Scanner(System.in)){
+					    //rest of your code
+						Integer includedClasses = studentInput.nextInt();
+
+					    if(includedClasses <100 || includedClasses >1) {
+							Parser parser = new Parser(p1.toString(), includedClasses.intValue());
+							parser.javaToJSonFile();
+					    } else  {
+					        System.out.println("invalid input :");
+
+					    }
+					}
 					
 				//	} else {
 						//System.out.println("Please provide input in correct format - <sourcefolder> <output file name>");
@@ -35,4 +47,5 @@ public class javaParser {
 	}
 
 }
+
 
