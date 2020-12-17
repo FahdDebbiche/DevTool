6d99bc074913d0539752dedef4d2ab46ceadc0fd
FahdDebbiche <gusdebfa@student.gu.se>
2020-12-18 00:15:18 +0100

    fix imports

--- src/analysis/Parser.java
+++ src/analysis/Parser.java
@@ -1,6 +1,5 @@
 package analysis;
 import java.io.BufferedReader;
-import java.io.Console;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -8,15 +7,16 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 import org.json.JSONArray;
